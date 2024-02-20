76c127967431472b1e7784e78793955cf3241d06
Jean-Pierre Höhmann <jean-pierre@hoehmann.jp>
2024-02-20 22:10:27 +0100

    Add license header to files

--- PluginProcessor.cpp
+++ PluginProcessor.cpp
@@ -1,3 +1,23 @@
+//
+// PluginProcessor.cpp
+// audio-plugin-example
+//
+// Created by Jean-Pierre Höhmann on 2024-02-20.
+// Copyright © 2024 Jean-Pierre Höhmann.
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
